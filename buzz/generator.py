02938b6533bec018fc70781583e5900afdeafc70
NoName19871 <smersh197@mail.ru>
2017-06-23 18:43:11 +0300

    fixed path to quotes

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,11 +1,19 @@
 import os
 import random
 
-def quotes_count(filename):
-	return sum(1 for i in open(filename, 'r'))
+def path_to_quotes():
+	path = os.getcwd()
+	if (path.endswith('cicd-buzz')):
+		path = 'buzz/quotes'
+	else:
+		path = '../buzz/quotes'
+	return path
+
+def quotes_count():
+	return sum(1 for i in open(path_to_quotes(), 'r'))
 
 def generate_buzz(n):
-	f = open(os.path.abspath('buzz/quotes'), 'r')
+	f = open(os.path.abspath(path_to_quotes()), 'r')
 	tmp = 0
 	for line in f:
 		if (tmp == n):
@@ -14,4 +22,4 @@ def generate_buzz(n):
 			tmp = tmp + 1
 
 if __name__ == "__main__":
-    print(generate_buzz(random.randrange(0, quotes_count(os.path.abspath('buzz/quotes')))))
+    print(generate_buzz(random.randrange(0, quotes_count())))
