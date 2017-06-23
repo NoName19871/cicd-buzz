fcf303d4ff2293316bfc000d125609a1aa3e79d7
NoName19871 <smersh197@mail.ru>
2017-06-23 18:02:48 +0300

    Added quotes

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,23 +1,17 @@
+import os
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+def quotes_count(filename):
+	return sum(1 for i in open(filename, 'r'))
 
-def sample(l, n = 1):
-    result = random.sample(l, n)
-    if n == 1:
-        return result[0]
-    return result
-
-def generate_buzz():
-    buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
-    return phrase.title()
+def generate_buzz(n):
+	f = open(os.path.abspath('buzz/quotes'), 'r')
+	tmp = 0
+	for line in f:
+		if (tmp == n):
+			return line[0:line.find('|')].title()
+		else:
+			tmp = tmp + 1
 
 if __name__ == "__main__":
-    print generate_buzz()
+    print(generate_buzz(random.randrange(0, quotes_count(os.path.abspath('buzz/quotes')))))
