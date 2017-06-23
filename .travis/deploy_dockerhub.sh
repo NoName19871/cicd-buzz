edcfa5852d389303de27fd9ecc2ae3be95b1c5ff
noname19871 <smersh197@mail.ru>
2017-06-23 11:31:56 +0300

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,5 +6,5 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag noname19871 zeroday343/cicd-buzz
-docker push noname19871/cicd-buzz:latest
+docker tag zeroday343 noname19871/cicd-buzz
+docker push zeroday343/cicd-buzz:latest
