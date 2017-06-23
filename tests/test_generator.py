544ba14bf08c291679079c3797065c11e2a6d9f8
NoName19871 <smersh197@mail.ru>
2017-06-23 18:10:30 +0300

    fix

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,5 +4,5 @@ import unittest
 from buzz import generator
 
 def test_title():
-    for x in range(0, generator.quotes_count(os.path.abspath('buzz/quotes'))):
+    for x in range(1, generator.quotes_count(os.path.abspath('buzz/quotes'))):
         assert generator.generate_buzz(x).istitle()
